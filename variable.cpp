5ac61c2d5790b2d012441fbeb1f19506275e758f
justMSARahat <justshamsulalom@gmail.com>
2025-02-09 15:51:16 +0600

    1 Hour Done, Easy Peasy

---
+++ variable.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int x; // Variable Declaration
+    x = 5; // Initialization
+    int y = 9; // Both - Variable Declaration and Initialization
+    cout<<x+y;
+}
